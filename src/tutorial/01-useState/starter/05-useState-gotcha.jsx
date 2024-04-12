f270ce8911e9d32c10fcbeb7e3856a79c944c22c
Bikal <bikaladhikari5@gmail.com>
2024-04-12 16:38:25 +1000

    UseState gotcha

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+  const [value, setValue] = useState(0);
 
+  const handleClick = () => {
+    // setValue(value + 1);
+    // //  be careful it's the old value
+    // console.log(value);
+    // //  so if you have any functionality
+    // // that relies on the latest value
+    // // it will be wrong !!!
+
+    setValue((currentState) => {
+      // must return otherwise undefined
+      // below is the latest/current state value
+      const newState = currentState + 1;
+      return newState;
+    });
+  };
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </div>
+  );
+};
 export default UseStateGotcha;
