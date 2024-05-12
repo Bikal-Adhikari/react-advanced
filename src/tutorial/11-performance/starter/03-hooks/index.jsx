1a5662bc0ff39cad028cc99609201d704950306d
Bikal <bikaladhikari5@gmail.com>
2024-05-12 19:47:10 +1000

    new items emphasied

--- src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,21 +1,46 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
-const LowerState = () => {
+import { useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
+const LowerStateChallenge = () => {
   const [people, setPeople] = useState(data);
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      alert("Please Provide Name Value");
+      return;
+    }
+    addPerson();
+    setName("");
+  };
+  const addPerson = () => {
+    const fakeId = Date.now();
+    const newPerson = { id: fakeId, name };
+    setPeople([...people, newPerson]);
+  };
   return (
     <section>
-      <button
-        className='btn'
-        onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
-      >
-        count {count}
-      </button>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-block" type="submit">
+          submit
+        </button>
+      </form>
       <List people={people} />
     </section>
   );
 };
-export default LowerState;
+export default LowerStateChallenge;
